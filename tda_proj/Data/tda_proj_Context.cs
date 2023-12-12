c98c8b8d78a211c14491c7f4404e9418f2bcc781
Jan Jedlička <jedlicka.honzik2000@email.cz>
2023-12-12 21:18:15 +0100

    commit od Jedličky

--- tda_proj/Data/tda_proj_Context.cs
+++ tda_proj/Data/tda_proj_Context.cs
@@ -18,7 +18,7 @@ namespace tda_proj.Data
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"Server=DESKTOP-8Q71K8S;Database=TdA;Integrated Security=true;Encrypt=True;TrustServerCertificate=True;");
+            optionsBuilder.UseSqlServer(@"Server=DESKTOP-M1F59UB\SQLEXPRESS;Database=TdA;Integrated Security=true;Encrypt=True;TrustServerCertificate=True;");
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
