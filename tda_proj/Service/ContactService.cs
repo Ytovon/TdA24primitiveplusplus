65a647cf50ef5f5cae6e57b839643c82971e1072
Jan Jedlička <jedlicka.honzik2000@email.cz>
2023-12-14 20:54:11 +0100

    První blazor commit Jedlička

---
+++ tda_proj/Service/ContactService.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using tda_proj.Data;
+using tda_proj.Model;
+
+
+namespace tda_proj.Service
+{
+    public class ContactService
+    {
+        // vrátit všechny kontakty s detailama (emaily a tel. čísla)
+        public List<Contact> GetAllContactsWithDetails()
+        {
+            using (tdaContext context = new tdaContext())
+            {
+                return context.Contacts
+                    .Include(c => c.Emails)
+                    .Include(c => c.TelNumbers)
+                    .ToList();
+            }
+        }
+
+    }
+}
---
+++ tda_proj/Service/TagService.cs
@@ -0,0 +1,21 @@
+﻿//using Microsoft.EntityFrameworkCore;
+//using System;
+//using tda_proj.Data;
+//using tda_proj.Model;
+
+//public class TagService
+//{
+//    public List<Tag> GetAllTags()
+//    { 
+//        using (tdaContext context = new tdaContext()) 
+//        {
+//            return context.Tags
+//                .Include(lt => lt.LectorTag)
+//                .ThenInclude(t => t.Tag)
+//                .ToList();
+//        }
+    
+//    }
+
+
+//}
