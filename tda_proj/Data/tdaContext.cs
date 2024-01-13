03753356a6cbe5ebe254ef281f379cb8c87dd716
zan_baklazan <125310811+Ytovon@users.noreply.github.com>
2024-01-13 21:42:52 +0100

    js added

--- tda_proj/Data/tdaContext.cs
+++ tda_proj/Data/tdaContext.cs
@@ -18,18 +18,18 @@ namespace tda_proj.Data
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             //var basePath = AppDomain.CurrentDomain.BaseDirectory;
-            optionsBuilder.UseSqlServer(@"Server=DESKTOP-H7G1B7O\SQLEXPRESS;Database=TdA;Integrated Security=true;Encrypt=True;TrustServerCertificate=True;");
+            optionsBuilder.UseSqlite(@"Data Source=wwwroot/tdaDB.db");
         }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             // Assuming Lector is your entity
             modelBuilder.Entity<Lector>()
                 .Property(l => l.UUID)
-                .HasDefaultValueSql("NEWID()");
+                .HasDefaultValueSql("lower(hex(randomblob(4))) || '-' || lower(hex(randomblob(2))) || '-' || '4' || substr(lower(hex(randomblob(2))), 2) || '-' || 'a' || substr(lower(hex(randomblob(2))), 2) || '-' || lower(hex(randomblob(6)))");
 
             modelBuilder.Entity<Tag>()
                 .Property(t => t.TagUUID)
-                .HasDefaultValueSql("NEWID()");
+                .HasDefaultValueSql("lower(hex(randomblob(4))) || '-' || lower(hex(randomblob(2))) || '-' || '4' || substr(lower(hex(randomblob(2))), 2) || '-' || 'a' || substr(lower(hex(randomblob(2))), 2) || '-' || lower(hex(randomblob(6)))");
 
             modelBuilder.Entity<LectorTag>()
                 .HasKey(e => new {e.LectorTagUUID, e.LectorUUID});
