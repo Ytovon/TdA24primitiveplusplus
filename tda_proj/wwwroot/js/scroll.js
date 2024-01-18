1fbb81a382e67881175a95d4dec31c4f12836d4d
Tomáš Vyhnálek <vyhnalektom27@gmail.com>
2024-01-18 15:32:46 +0100

    uprava css

--- tda_proj/wwwroot/js/scroll.js
+++ tda_proj/wwwroot/js/scroll.js
@@ -1,31 +1,73 @@
-﻿var scrollsize = 250;
+﻿window.onload = function () {
+    // Wrap your code in a function to avoid polluting the global namespace
+    (function () {
+        var scrollsize = 250;
 
-function scrollLeftSmooth(element) {
-    setTimeout(() => {
-        element.scrollTo({
-            left: element.scrollLeft - scrollsize,
-            behavior: 'smooth'
+        function scrollLeftSmooth(element) {
+            setTimeout(() => {
+                element.scrollTo({
+                    left: element.scrollLeft - scrollsize,
+                    behavior: 'smooth'
+                });
+            }, 80);
+        }
+
+        function scrollRightSmooth(element) {
+            setTimeout(() => {
+                element.scrollTo({
+                    behavior: 'smooth',
+                    left: element.scrollLeft + scrollsize,
+                });
+            }, 80);
+        }
+
+        document.querySelectorAll('.tags').forEach(function (element) {
+            var prevButton = element.querySelector('.prev_btn');
+            var nextButton = element.querySelector('.next_btn');
+
+            prevButton.addEventListener('click', function () {
+                scrollLeftSmooth(element);
+            });
+
+            nextButton.addEventListener('click', function () {
+                scrollRightSmooth(element);
+            });
         });
-    }, 80); // Delay of 1000 milliseconds (1 second)
-}
+    })();
+};
 
-function scrollRightSmooth(element) {
-    setTimeout(() => {
-        element.scrollTo({
-            behavior: 'smooth',
-            left: element.scrollLeft + scrollsize, // Adjust the scroll distance as needed
+
+window.initializeScrolling = function (element) {
+    var scrollsize = 250;
+
+    function scrollLeftSmooth(element) {
+        setTimeout(() => {
+            element.scrollTo({
+                left: element.scrollLeft - scrollsize,
+                behavior: 'smooth'
+            });
+        }, 80);
+    }
+
+    function scrollRightSmooth(element) {
+         setTimeout(() => {
+                element.scrollTo({
+                    behavior: 'smooth',
+                    left: element.scrollLeft + scrollsize,
+                });
+            }, 80);
+    }
+
+    document.querySelectorAll('.tags').forEach(function (element) {
+        var prevButton = element.querySelector('.prev_btn');
+        var nextButton = element.querySelector('.next_btn');
+
+        prevButton.addEventListener('click', function () {
+            scrollLeftSmooth(element);
         });
-    }, 80); // Delay of 1000 milliseconds (1 second)
-}
-document.querySelectorAll('.tags').forEach(function (element) {
-    var prevButton = element.querySelector('.prev_btn');
-    var nextButton = element.querySelector('.next_btn');
 
-    prevButton.addEventListener('click', function () {
-        scrollLeftSmooth(element);
+        nextButton.addEventListener('click', function () {
+            scrollRightSmooth(element);
+        });
     });
-
-    nextButton.addEventListener('click', function () {
-        scrollRightSmooth(element);
-    });
-});
+};
