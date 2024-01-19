454a874caba37c3e8e281b6255e9f1647c3a4e24
Tomáš Vyhnálek <vyhnalektom27@gmail.com>
2024-01-19 11:45:02 +0100

    přidání Price Range slideru

--- tda_proj/obj/Debug/net7.0/tda_proj.AssemblyInfo.cs
+++ tda_proj/obj/Debug/net7.0/tda_proj.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tda_proj")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1fbb81a382e67881175a95d4dec31c4f12836d4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dfdf012a04b48285330da4a8ab01d29d66088712")]
 [assembly: System.Reflection.AssemblyProductAttribute("tda_proj")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tda_proj")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
